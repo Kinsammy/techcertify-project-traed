c1a6eed47966fac7c5ca6af7bad5f76676da4cf3
Kinsammy <fanusamuel@gmail.com>
2024-02-11 16:40:32 +0100

    Navbar in progress...

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './container/pages/Navbar';
+// import './index.css';
+
+
+function App() {
+  return (
+   <div>
+    <Navbar />
+   </div>
+  )
+}
+
+export default App;
