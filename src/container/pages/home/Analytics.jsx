4a25826da06b1502fff6deccf4cffa3904dce818
Kinsammy <fanusamuel@gmail.com>
2024-02-14 22:22:02 +0100

    Hero in progress...

---
+++ src/container/pages/home/Analytics.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Analytics = () => {
+  return (
+    <div className="w-full px-4 py-16 text-white">
+      <div className="pt-8 mx-auto">
+        <p>Traed helps your business save time every day!</p>
+        <p>
+          Traed offers a simple and clear accounting software that can be used
+          anywhere at any. This allows a maximum of flexibility and convenience
+          for you and your customers.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Analytics;
--- src/container/pages/home/Home.jsx
+++ src/container/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
+import Analytics from "./Analytics";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Analytics />
     </div>
   );
 };
