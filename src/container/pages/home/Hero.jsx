83d9bebfea5e5ac1eb740015feab95530935bd2b
Kinsammy <fanusamuel@gmail.com>
2024-02-15 14:04:47 +0100

    Hero Section in progress...

--- src/container/pages/home/Hero.jsx
+++ src/container/pages/home/Hero.jsx
@@ -3,25 +3,36 @@ import HeroImg from "../../../assets/image/jpeg/hero.jpeg";
 
 const Hero = () => {
   return (
-    <div className="text-[#0C2027]">
-      <div className="w-full h-screen relatve">
-        <div className="absolute">
-          <img src={HeroImg} alt="" />
-        </div>
-        <div className="absolute max-w-[800px] pl-16">
-          <p className="font-bold text-[68px]  mt-[10rem] ">
+    <div className=" relatve text-[#0C2027]">
+      <div
+        className="relative overflow-hidden bg-cover  bg-no-repeat p-12 text-center"
+        style={{
+          backgroundImage: `url(${HeroImg})`,
+          height: "700px",
+        }}>
+        <div
+          className="absolute inset-0"
+          style={{
+            background:
+              "linear-gradient(to right, rgba(255,255,255,0), #FFFFFF 300%)",
+            zIndex: 0,
+            width: "10%",
+          }}></div>
+
+        <div className="absolute inset-0 flex flex-col justify-center max-w-[800px] pl-16">
+          <p className="font-bold text-6xl  mt-40">
             Accounting, but make it easy - withTraed!
           </p>
-          <p className="text-[#7E8693]">
+          <p className="text-[#7E8693] mt-4">
             Applications that power financial institutions, marketplaces that
             accelerate industry & an open innovation platform for banks,
             fintechs & non-banks to connect and collaborate.
           </p>
-          <div className="">
+          <div className="flex items-center mt-8">
             <button className="bg-[#06BA63] text-[#FFFFFF] w-[200px] rounded-md font-medium my-6 mx-auto py-3">
-              Get Started <span classname="text-[]">&#8594;</span>
+              Get Started <span classname="size={5}">&#8594;</span>
             </button>
-            <p className="#6D758F">No credit card necessary. Its free!!</p>
+            <p className="#6D758F mt-2">No credit card necessary. Its free!!</p>
           </div>
         </div>
       </div>
