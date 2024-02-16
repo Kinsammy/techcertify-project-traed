dbfa64c32260aabd2c34457c9bff842964954bc8
Kinsammy <fanusamuel@gmail.com>
2024-02-17 00:40:41 +0100

    Hero Section in progress...

--- src/container/pages/home/Hero.jsx
+++ src/container/pages/home/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HeroImg from "../../../assets/image/jpeg/hero.jpeg";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Hero = () => {
   return (
@@ -11,28 +12,30 @@ const Hero = () => {
           height: "700px",
         }}>
         <div
-          className="absolute inset-0"
+          className="absolute inset-1"
           style={{
             background:
-              "linear-gradient(to right, rgba(255,255,255,0), #FFFFFF 200%)",
-            zIndex: 0,
-            width: "40%",
+              "linear-gradient(to right, rgba(255,255,255,0), #FFFFFF 300%)",
+            zIndex: 2,
+            width: "100%",
           }}></div>
 
-        <div className="absolute inset-0 flex flex-col justify-center max-w-[800px] pl-16">
-          <p className="font-bold text-6xl  mt-40">
+        <div className="absolute inset-0 flex flex-col justify-center max-w-[800px] ml-16 px-4 text-left">
+          <h1 className="font-bold text-6xl  mt-30">
             Accounting, but make it easy - withTraed!
-          </p>
-          <p className="text-[#7E8693] mt-4">
+          </h1>
+          <p className="text-[#7E8693] mt-8">
             Applications that power financial institutions, marketplaces that
             accelerate industry & an open innovation platform for banks,
             fintechs & non-banks to connect and collaborate.
           </p>
-          <div className="flex flex-row  mt-8">
-            <button className="bg-[#06BA63] text-[#FFFFFF] w-[200px] rounded-md font-medium my-6 mx-auto py-3">
-              Get Started <span classname="size={5}">&#8594;</span>
+          <div className="flex items-center mt-8">
+            <button className="bg-[#06BA63] text-[#FFFFFF] w-[150px] rounded-md  font-medium py-2 flex items-center justify-center">
+              Get Started <AiOutlineArrowRight className="ml-1" />
             </button>
-            <p className="#6D758F mt-2">No credit card necessary. Its free!!</p>
+            <p className="#6D758F mt-2 ml-8">
+              No credit card necessary. Its free!!
+            </p>
           </div>
         </div>
       </div>
