550f0d3090df6717cb25b0839f290ae3b91ffe06
Kinsammy <fanusamuel@gmail.com>
2024-02-17 00:54:33 +0100

    Hero Section in progress...

--- src/container/pages/home/Navbar.jsx
+++ src/container/pages/home/Navbar.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../../assets/image/svg/logo.svg";
+import {
+  AiOutlineArrowRight,
+  AiOutlineClose,
+  AiOutlineMenu,
+} from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="">
       <header className="flex items-center justify-between h-24 max-w-[1240px] mx-auto px-14 text-white">
@@ -51,55 +62,12 @@ const Navbar = () => {
             <a href="">Company</a>
           </li>
         </ul>
-        <button className="bg-[#06BA63] text-[#FFFFFF] w-[300px] rounded-md font-medium my-6 mx-auto py-3">
-          Get Started <span classname="text-[5px]">&#8594;</span>
+        <button className="bg-[#06BA63] text-[#FFFFFF] w-[300px] rounded-md font-bold mt-4  py-2 flex items-center justify-center">
+          Get Started <AiOutlineArrowRight className="ml-1" />
         </button>
-        {/* <div className="flex space-x-4 mt-2">
-          <div>
-            <a href="/" className="active">
-              About
-            </a>
-            <a className="text-[#F06523]" href="/">
-              Our Solutions
-            </a>
-            <div className="relative group">
-              <a href="/" className="flex items-center">
-                Sectors
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 ml-1 transform transition-transform duration-200 group-hover:rotate-180"
-                  viewBox="0 0 20 20"
-                  fill="currentColor">
-                  <path
-                    fillRule="evenodd"
-                    d="M10 14a1 1 0 01-.707-.293l-4-4a1 1 0 111.414-1.414L10 12.586l3.293-3.293a1 1 0 111.414 1.414l-4 4A1 1 0 0110 14z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </a>
-              <div className="absolute hidden group-hover:block bg-black shadow-md py-2 rounded-lg mt-2 ">
-                <a href="#" className="block px-4 py-2">
-                  Sector 1
-                </a>
-                <a href="#" className="block px-4 py-2">
-                  Sector 2
-                </a>
-                <a href="#" className="block px-4 py-2">
-                  Sector 3
-                </a>
-              </div>
-            </div>
-
-            <a href="/">Knowledge</a>
-            <a href="/">Company</a>
-          </div>
-
-          <a
-            href="#"
-            className="flex items-center rounded-md bg-green-600  px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-            Get started <span classname="m1-1">&#8594;</span>
-          </a>
-        </div> */}
+        <div onClick={handleNav} className="block md:hidden sm:hidden">
+          {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        </div>
       </header>
     </div>
   );
