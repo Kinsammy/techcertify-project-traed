ca8191f3d63643d1e169e88ede570b27dcc27449
Kinsammy <fanusamuel@gmail.com>
2024-02-17 11:13:03 +0100

    Hero Section in progress...

--- src/container/pages/home/Analytics.jsx
+++ src/container/pages/home/Analytics.jsx
@@ -3,14 +3,18 @@ import React from "react";
 const Analytics = () => {
   return (
     <div className="w-full px-4 py-16 text-white">
-      <div className="pt-8 mx-auto">
-        <p>Traed helps your business save time every day!</p>
-        <p>
+      <div className="py-8 mx-auto max-w-[800px] text-center ">
+        <h2 className="w-[500px] text-4xl mx-auto">
+          Traed helps your business save time every day!
+        </h2>
+        <p className="text-1xl mt-5 h-3">
           Traed offers a simple and clear accounting software that can be used
           anywhere at any. This allows a maximum of flexibility and convenience
           for you and your customers.
         </p>
       </div>
+
+      <div></div>
     </div>
   );
 };
