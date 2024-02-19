99d91d7ddeade6a088e2cf25e49b0e6bc381e80d
Kinsammy <fanusamuel@gmail.com>
2024-02-19 15:09:23 +0100

    Solution Section in progress...

--- src/container/pages/home/About.jsx
+++ src/container/pages/home/About.jsx
@@ -3,18 +3,20 @@ import AboutImg from "../../../assets/image/svg/about.svg";
 
 const About = () => {
   return (
-    <div className="w-full text-white px-4 py-12">
-      <div className="flex">
-        <div className=" w-[50%] py-16 mt-14">
-          <h3>About Us</h3>
-          <p>
+    <div className="w-full text-white px-4 py-16">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+        <div className="flex flex-col justify-center md:mx-auto sm:mx-auto">
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-6">
+            About Us
+          </h1>
+          <p className="w-[500px]">
             TraedAI is a trade finance platform that leverages cutting-edge AI
             and machine learning technology to provide innovative solutions to
             businesses looking to optimize their trade finance processes.
           </p>
         </div>
-        <div className=" w-[50%]">
-          <img className="" src={AboutImg} alt="/" />
+        <div className="mx-auto my-4 mr-6 md:mx-auto sm:mx-auto">
+          <img className="w-[500px]" src={AboutImg} alt="/" />
         </div>
       </div>
     </div>
---
+++ src/container/pages/home/Solution.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Solution = () => {
+  return (
+    <div className="">
+      <div></div>
+    </div>
+  );
+};
+
+export default Solution;
