0ea95ee12adaf0d59f7ad75546401360c3123da6
Kinsammy <fanusamuel@gmail.com>
2024-02-20 12:00:41 +0100

    Management Section in progress...

--- src/container/pages/home/Testimonials.jsx
+++ src/container/pages/home/Testimonials.jsx
@@ -7,15 +7,15 @@ const Testimonials = () => {
       <h2 className="md:text-4xl sm:text-3xl text-2xl font-bold py-6">
         What our users say?
       </h2>
-      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2 ">
-        <div className="w-[500px] px-10">
+      <div className="max-w-[1240px] grid md:grid-cols-2 ">
+        <div className="w-[500px] mx-auto p-10 bg-[#7E86931A] rounded-3xl">
           <p>
             “Traed has been a game-changer for our business. Their factoring
             services have improved our cash flow, allowing us to focus on growth
             and expansion. I recommend Traed to any business looking for
             reliable financial solutions.”
           </p>
-          <div className="flex flex-row py-8">
+          <div className="flex flex-row pt-8">
             <img className="pr-2" src={TestimonialImg} alt="/" />
             <div className="p-4">
               <h4>John carter</h4>
@@ -23,13 +23,13 @@ const Testimonials = () => {
             </div>
           </div>
         </div>
-        <div className="w-[400px]">
-          <p>
+        <div className="w-[500px] mx-auto p-10 bg-[#7E86931A] rounded-3xl">
+          <p className="px-2">
             “The Traed team's expertise in the industry is evident, and their
             commitment to our success is unmatched. We're grateful for their
             support and highly recommend their services.”
           </p>
-          <div className="flex flex-row py-6">
+          <div className="flex flex-row pt-6">
             <img className="pr-2" src={TestimonialImg} alt="/" />
             <div className="p-4">
               <h4>John carter</h4>
