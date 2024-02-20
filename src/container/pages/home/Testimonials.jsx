53860ae3d27221376e53e3c7a2c377b80ecf9bc9
Kinsammy <fanusamuel@gmail.com>
2024-02-20 11:48:15 +0100

    Management Section in progress...

--- src/container/pages/home/Testimonials.jsx
+++ src/container/pages/home/Testimonials.jsx
@@ -7,32 +7,31 @@ const Testimonials = () => {
       <h2 className="md:text-4xl sm:text-3xl text-2xl font-bold py-6">
         What our users say?
       </h2>
-      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
-        <div>
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2 ">
+        <div className="w-[500px] px-10">
           <p>
             “Traed has been a game-changer for our business. Their factoring
             services have improved our cash flow, allowing us to focus on growth
             and expansion. I recommend Traed to any business looking for
             reliable financial solutions.”
           </p>
-          <div>
-            <img src={TestimonialImg} alt="/" />
-            <div>
+          <div className="flex flex-row py-8">
+            <img className="pr-2" src={TestimonialImg} alt="/" />
+            <div className="p-4">
               <h4>John carter</h4>
               <h5>Role, Company</h5>
             </div>
           </div>
         </div>
-        <div className="">
+        <div className="w-[400px]">
           <p>
-            “Traed has been a game-changer for our business. Their factoring
-            services have improved our cash flow, allowing us to focus on growth
-            and expansion. I recommend Traed to any business looking for
-            reliable financial solutions.”
+            “The Traed team's expertise in the industry is evident, and their
+            commitment to our success is unmatched. We're grateful for their
+            support and highly recommend their services.”
           </p>
           <div className="flex flex-row py-6">
-            <img className="pr-6" src={TestimonialImg} alt="/" />
-            <div>
+            <img className="pr-2" src={TestimonialImg} alt="/" />
+            <div className="p-4">
               <h4>John carter</h4>
               <h5>Role, Company</h5>
             </div>
