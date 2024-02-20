ffc65efb1435d021bed49f88654b83bae91c09c3
Kinsammy <fanusamuel@gmail.com>
2024-02-20 18:20:16 +0100

    Footer Section in progress...

--- src/container/pages/home/Hero.jsx
+++ src/container/pages/home/Hero.jsx
@@ -4,7 +4,7 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Hero = () => {
   return (
-    <div className=" relatve text-[#0C2027]">
+    <div className="relatve text-[#0C2027]">
       <div
         className="relative overflow-hidden bg-cover  bg-no-repeat p-12 text-center"
         style={{
@@ -20,20 +20,20 @@ const Hero = () => {
             width: "100%",
           }}></div>
 
-        <div className="absolute inset-0 flex flex-col justify-center max-w-[800px] ml-16 px-4 text-left">
-          <h1 className="font-bold text-6xl  mt-30">
+        <div className="absolute inset-0 flex flex-col justify-center w-[53rem] px-24  text-left">
+          <h1 className="font-bold text-6xl  pt-30 ">
             Accounting, but make it easy - withTraed!
           </h1>
-          <p className="text-[#7E8693] mt-8">
+          <p className="text-[#7E8693] text-sm pr-19 py-8 ">
             Applications that power financial institutions, marketplaces that
             accelerate industry & an open innovation platform for banks,
             fintechs & non-banks to connect and collaborate.
           </p>
           <div className="flex items-center mt-8">
-            <button className="bg-[#06BA63] text-[#FFFFFF] w-[150px] rounded-md  font-medium py-2 flex items-center justify-center">
+            <button className="bg-[#06BA63] text-[#FFFFFF] w-[120px] rounded-md  font-medium py-2 flex items-center justify-center">
               Get Started <AiOutlineArrowRight className="ml-1" />
             </button>
-            <p className="#6D758F mt-2 ml-8">
+            <p className="#6D758F ml-2 py-4 text-sm">
               No credit card necessary. Its free!!
             </p>
           </div>
--- src/container/pages/home/Navbar.jsx
+++ src/container/pages/home/Navbar.jsx
@@ -14,57 +14,64 @@ const Navbar = () => {
   };
 
   return (
-    <div className="">
-      <header className="relative z-10 flex items-center justify-between h-24 max-w-[1240px] mx-auto px-4 text-white">
-        <a href="/" className="w-full">
-          <img src={Logo} alt="Logo" />
-        </a>
-        <ul className="hidden md:flex text-[16px] font-bold mt-4 mr-12">
-          <li className="p-4">
-            <a href="/">About</a>
-          </li>
-          <li className="p-4 text-[#F06523] whitespace-nowrap">
-            <a href="/">Our Solutions</a>
-          </li>
-          <li className="p-4">
-            <div className="relative group">
-              <a href="/" className="flex items-center">
-                Sectors
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 ml-1 transform transition-transform duration-200 group-hover:rotate-180"
-                  viewBox="0 0 20 20"
-                  fill="currentColor">
-                  <path
-                    fillRule="evenodd"
-                    d="M10 14a1 1 0 01-.707-.293l-4-4a1 1 0 111.414-1.414L10 12.586l3.293-3.293a1 1 0 111.414 1.414l-4 4A1 1 0 0110 14z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </a>
-              <div className="absolute hidden group-hover:block bg-[#0c2027] text-center w-[300px] shadow-md py-2 rounded-lg mt-2 ">
-                <a href="#" className="block px-4 py-2 border-b border-white">
-                  Sector 1
+    <div className=" text-white px-28 bg-[#0C2027]">
+      <header className="max-w-[1240px] px-8  relative z-10 flex items-center justify-between h-24">
+        <div>
+          <a href="/">
+            <img className="w-full" src={Logo} alt="Logo" />
+          </a>
+        </div>
+        <div className="flex items-center">
+          <ul className="hidden md:flex text-[16px] font-bold">
+            <li className="p-4">
+              <a href="/">About</a>
+            </li>
+            <li className="p-4 text-[#F06523] whitespace-nowrap">
+              <a href="/">Our Solutions</a>
+            </li>
+            <li className="p-4">
+              <div className="relative group">
+                <a href="/" className="flex items-center">
+                  Sectors
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 ml-1 transform transition-transform duration-200 group-hover:rotate-180"
+                    viewBox="0 0 20 20"
+                    fill="currentColor">
+                    <path
+                      fillRule="evenodd"
+                      d="M10 14a1 1 0 01-.707-.293l-4-4a1 1 0 111.414-1.414L10 12.586l3.293-3.293a1 1 0 111.414 1.414l-4 4A1 1 0 0110 14z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
                 </a>
-                <a href="#" className="block px-4 py-2 border-b border-white">
-                  Sector 2
-                </a>
-                <a href="#" className="block px-4 py-2 border-b border-white">
-                  Sector 3
-                </a>
+                <div className="absolute hidden group-hover:block bg-[#0c2027] text-center w-[300px] shadow-md py-2 rounded-lg mt-2 ">
+                  <a href="#" className="block px-4 py-2 border-b border-white">
+                    Sector 1
+                  </a>
+                  <a href="#" className="block px-4 py-2 border-b border-white">
+                    Sector 2
+                  </a>
+                  <a href="#" className="block px-4 py-2 border-b border-white">
+                    Sector 3
+                  </a>
+                </div>
               </div>
-            </div>
-          </li>
-          <li className="p-4">
-            <a href="">Knowledge</a>
-          </li>
-          <li className="p-4">
-            <a href="">Company</a>
-          </li>
-        </ul>
-        <button className="hidden md:flex bg-[#06BA63] text-[#FFFFFF] w-[300px] rounded-md font-bold mt-4  py-2 items-center justify-center">
-          Get Started <AiOutlineArrowRight className="ml-1" />
-        </button>
+            </li>
+            <li className="p-4">
+              <a href="">Knowledge</a>
+            </li>
+            <li className="p-4">
+              <a href="">Company</a>
+            </li>
+          </ul>
+          <div className="p-2">
+            <button className="hidden md:flex bg-[#06BA63] text-[#FFFFFF] w-[150px] rounded-xl font-bold   p-2 items-center justify-center">
+              Get Started <AiOutlineArrowRight className="ml-1" />
+            </button>
+          </div>
+        </div>
+
         <div onClick={handleNav} className="block md:hidden">
           {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
         </div>
@@ -74,7 +81,7 @@ const Navbar = () => {
               ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#0c2027] p-12 ease-in-out duration-500"
               : "fixed left-[-100%]"
           }>
-          <a href="/" className="w-full p-4 ">
+          <a href="/">
             <img src={Logo} alt="Logo" />
           </a>
           <ul className="uppercase p-4">
