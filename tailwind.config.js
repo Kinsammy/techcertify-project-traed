121af44363c8b2d2f358e44e74bfb7d0ef2e5277
Kinsammy <fanusamuel@gmail.com>
2024-02-21 23:43:00 +0100

    responsivesnes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "547px",
+      lg: "768px",
+      xl: "1024px",
+      "2xl": "1680px",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
